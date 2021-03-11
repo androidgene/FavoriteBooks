757c081ce582a196829c40ca638c48f8f0c1fcb2
androidgene <burns.gene@gmail.com>
2021-03-10 18:45:21 -0600

    update 1

--- app/src/main/java/com/devslopes/favoritebooks/AddBookActivity.kt
+++ app/src/main/java/com/devslopes/favoritebooks/AddBookActivity.kt
@@ -4,6 +4,7 @@ package com.devslopes.favoritebooks
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.devslopes.favoritebooks.databinding.AddBookBinding
+import com.devslopes.favoritebooks.models.Book
 
 class AddBookActivity: AppCompatActivity() {
 
@@ -13,7 +14,12 @@ class AddBookActivity: AppCompatActivity() {
         setContentView(binding.root)
 
         binding.saveButton.setOnClickListener {
-
+            BookRepository.addBook(enterItem(binding), this)
         }
+    }
+
+    fun enterItem(binding: AddBookBinding): Book {
+        val newItem: String = "${binding.title.text.toString()}|${binding.author.text.toString()}|${binding.genre.text.toString()}| ${binding.length.text.toString()}"
+        return Book.fromCsv(newItem)
     }
 }
